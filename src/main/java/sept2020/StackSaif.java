2b7e6f996e7f655ee2baa04fb63e176561eb36bb
Saiful Abu <saifulabu25@gmail.com>
2020-10-08 22:42:23 -0500

    adding stack implementation using array

--- src/main/java/sept2020/StackSaif.java
+++ src/main/java/sept2020/StackSaif.java
@@ -3,18 +3,46 @@ package sept2020;
 import contracts.AbstractStack;
 
 public class StackSaif extends AbstractStack {
-  @Override
-  public void push(int data) {
+  int initialCapacity = 2;
+  int[] data;
+  int nextPointer = 0;
+  int curSize = 0;
 
+  public StackSaif() {
+    data = new int[initialCapacity];
+  }
+
+  @Override
+  public void push(int input) {
+    if(curSize == data.length) {
+      int nextSize = 2 * data.length;
+      int[] newData = new int[nextSize];
+      copy(data, newData);
+      data = newData;
+    }
+    data[nextPointer] = input;
+    nextPointer++;
+    curSize++;
   }
 
   @Override
   public int pop() {
-    return 0;
+    if(curSize == 0) {
+      throw new IllegalStateException("stack underflow");
+    }
+    nextPointer--;
+    curSize--;
+    return data[nextPointer];
   }
 
   @Override
   public int size() {
-    return 0;
+    return curSize;
+  }
+
+  private void copy(int[] oldData, int[] newData) {
+    for(int i = 0; i < oldData.length; i++) {
+      newData[i] = oldData[i];
+    }
   }
 }
